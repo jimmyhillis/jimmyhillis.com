bd5d8dc8d4450d0419139ecd6338cb04304bf660
Jimmy Hillis <jimmy.hillis@me.com>
2012-05-18 09:25:49 +0800

    Adding a setting for my static routes to help caching

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('static', __dirname + '/public')
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(app.set('static')));
 });
 
 app.configure('development', function(){
@@ -36,6 +37,6 @@ app.get('/lab', routes.lab);
 app.get('/contact', routes.contact);
 app.get('/last-fm', routes.lastfm_feed);
 
-app.listen(80, function(){
+app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
