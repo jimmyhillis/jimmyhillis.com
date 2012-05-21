429aea63c7cb83f46fa2218646a230e1cb5e4285
Jimmy Hillis <jimmy.hillis@me.com>
2012-05-21 23:18:58 +0800

    Removing non-tweet related code into ui.js

---
+++ public/javascripts/ui.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+	// INSTAGRAM LATEST PHOTOS
+
+	var appendPhotos = function appendPhotos(photos) {
+
+		console.log("Yeah man!");
+
+		var $instagram_feed = $('.instagram-feed')
+			, $photo_list = $('<ul></ul>')
+					.addClass('instagram-photos') // list of all photos to build before adding
+			, $photo // single photo li element
+			, $image // photo element to add to stream
+			, i // iterator for the loop
+		;
+
+		if (photos.length) {
+
+			for (i = photos.length - 1; i > 0; i--) {
+				console.log(photos[i].images.thumbnail.url);
+				$photo = $('<img />').addClass('instagram-photo').attr('src', photos[i].images.thumbnail.url);
+				$photo_list.append($('<li></li>').append($photo));
+			}
+
+			$instagram_feed.append($photo_list);
+
+		}
+
+	}
+
+	loadJSONP("/instagram", appendPhotos);
+
+	// LAST.FM RECENTLY PLAYED LIST
+
+	var appendPlays = function appendPlays(plays) {
+
+		var $lastfm_feed = $('.last-fm-feed')
+			, $plays_list = $('<ul></ul>').addClass('last-fm-plays') // the list of plays you can append to your HTML
+			, $play // a specific play li element
+			, i // the iterator for the for loop
+		;
+
+		if (plays.length) {
+
+			for (i = plays.length - 1; i > 0; i--) {
+				$play = $('<li></li>')
+					.addClass('last-fm-play')
+						.addClass('well')
+							.text(plays[i]);
+				$plays_list.append($play);
+			}
+
+			$lastfm_feed.append($plays_list);
+
+		}
+
+	}
+
+	// Load LAST.FM RECENT TRACKS LIST
+	loadJSONP("/last-fm", appendPlays);
+
+});
