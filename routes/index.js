c96ee57a4621d3f548025806c01bc33d38e308d7
Jimmy Hillis <jimmy.hillis@me.com>
2012-05-23 07:57:32 +0800

    Turning route into a function to pass app data

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,70 @@
-
 /* 
- * Load required Routes for the site
+ * Index controllers are used to build basic
+ * content pages (with unique page designs) from 
+ * my database.
  */
 
-exports.index = function(req, res){
-	var content;
-	
-	content = _parseMarkdown(__dirname + '/../public/content/index.md');
-  res.render('index', { title: 'jimmy.hillis.me', blog: content })
-};
+module.exports = function (app) {
 
-exports.lab = function(req, res){
-	var content;
-	
-	content = _parseMarkdown(__dirname + '/../public/content/lab.md');
-	res.render('lab', { title: 'Lab', content: content })
-};
+	var controller = {};
+	db = app.set('db');
 
-exports.folio = function(req, res){
-	res.render('folio', { title: 'Web folio' })
-}
+	controller.index = function(req, res){
+	  var content;
 
-exports.contact = function(req, res){
-	var content;
-	
-	content = _parseMarkdown(__dirname + '/../public/content/contact.md');
-	res.render('contact', { title: 'Contact', content: content })
-};
+	  content = _parseMarkdownFile(__dirname + '/../public/content/index.md');
+	  res.render('index', { title: 'jimmy.hillis.me', blog: content })
+	};
 
-function _parseMarkdown(markdown_file) {
+	controller.lab = function(req, res){
+		var content;
+		
+		content = _parseMarkdownFile(__dirname + '/../public/content/lab.md');
+		res.render('lab', { title: 'Lab', content: content })
+	};
 
-	var fs = require('fs')
-		, markdown = require('markdown').markdown;
+	controller.folio = function(req, res){
+		res.render('folio', { title: 'Web folio' })
+	} // !controller.folio
 
-	try {
-		var markdown_str = fs.readFileSync(markdown_file, 'ascii');
-	} catch (err) {
-		console.log("Error loading the Blog file");
+	controller.contact = function(req, res) {
+
+		var content = ""
+			, Pages = db.model('Pages');
+
+	  // Load the MD content for this page
+	  Pages.findOne({ 'title' : 'Contact' }, function(err, this_page) { 
+
+			if (err) {
+				console.log("Loading content error");
+			}
+
+			content = _parseMarkdown(this_page.copy);
+			res.render('contact', { title: 'Contact', content: content })
+
+	  });
+		
+	}; // !controller.contact
+
+	// Private methods
+
+	function _parseMarkdown(markdown_str) {
+		var markdown = require('markdown').markdown;
+		return markdown.toHTML(markdown_str);
 	}
 
-	return markdown.toHTML(markdown_str);
+	function _parseMarkdownFile(markdown_file) {
+		var fs = require('fs')
+			, markdown = require('markdown').markdown;
 
-}
+		try {
+			var markdown_str = fs.readFileSync(markdown_file, 'ascii');
+		} catch (err) {
+			console.log("Error loading the Blog file");
+		}
+
+		return markdown.toHTML(markdown_str);
+	} // !_parseMarkdownFile
+
+  return controller;
+}	
