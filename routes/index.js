c1169bbe59cc738b7496ed54d158f2e497a34e88
Jimmy Hillis <jimmy.hillis@me.com>
2012-05-23 08:19:16 +0800

    Adding a new post on Mongoose object sharing

--- routes/index.js
+++ routes/index.js
@@ -4,67 +4,66 @@
  * my database.
  */
 
-module.exports = function (app) {
+var controller = {};
 
-	var controller = {};
+module.exports = function (app) {	
 	db = app.set('db');
+	return controller;
+}
 
-	controller.index = function(req, res){
-	  var content;
+controller.index = function(req, res){
+  var content;
 
-	  content = _parseMarkdownFile(__dirname + '/../public/content/index.md');
-	  res.render('index', { title: 'jimmy.hillis.me', blog: content })
-	};
+  content = _parseMarkdownFile(__dirname + '/../public/content/index.md');
+  res.render('index', { title: 'jimmy.hillis.me', blog: content })
+};
 
-	controller.lab = function(req, res){
-		var content;
-		
-		content = _parseMarkdownFile(__dirname + '/../public/content/lab.md');
-		res.render('lab', { title: 'Lab', content: content })
-	};
+controller.lab = function(req, res){
+	var content;
+	
+	content = _parseMarkdownFile(__dirname + '/../public/content/lab.md');
+	res.render('lab', { title: 'Lab', content: content })
+};
 
-	controller.folio = function(req, res){
-		res.render('folio', { title: 'Web folio' })
-	} // !controller.folio
+controller.folio = function(req, res){
+	res.render('folio', { title: 'Web folio' })
+} // !controller.folio
 
-	controller.contact = function(req, res) {
+controller.contact = function(req, res) {
 
-		var content = ""
-			, Pages = db.model('Pages');
+	var content = ""
+		, Pages = db.model('Pages');
 
-	  // Load the MD content for this page
-	  Pages.findOne({ 'title' : 'Contact' }, function(err, this_page) { 
+  // Load the MD content for this page
+  Pages.findOne({ 'title' : 'Contact' }, function(err, this_page) { 
 
-			if (err) {
-				console.log("Loading content error");
-			}
-
-			content = _parseMarkdown(this_page.copy);
-			res.render('contact', { title: 'Contact', content: content })
-
-	  });
-		
-	}; // !controller.contact
-
-	// Private methods
-
-	function _parseMarkdown(markdown_str) {
-		var markdown = require('markdown').markdown;
-		return markdown.toHTML(markdown_str);
-	}
-
-	function _parseMarkdownFile(markdown_file) {
-		var fs = require('fs')
-			, markdown = require('markdown').markdown;
-
-		try {
-			var markdown_str = fs.readFileSync(markdown_file, 'ascii');
-		} catch (err) {
-			console.log("Error loading the Blog file");
+		if (err) {
+			console.log("Loading content error");
 		}
 
-		return markdown.toHTML(markdown_str);
-	} // !_parseMarkdownFile
+		content = _parseMarkdown(this_page.copy);
+		res.render('contact', { title: this_page.title, content: content })
 
-  return controller;
-}	
+  });
+	
+}; // !controller.contact
+
+// Private methods
+
+function _parseMarkdown(markdown_str) {
+	var markdown = require('markdown').markdown;
+	return markdown.toHTML(markdown_str);
+}
+
+function _parseMarkdownFile(markdown_file) {
+	var fs = require('fs')
+		, markdown = require('markdown').markdown;
+
+	try {
+		var markdown_str = fs.readFileSync(markdown_file, 'ascii');
+	} catch (err) {
+		console.log("Error loading the Blog file");
+	}
+
+	return markdown.toHTML(markdown_str);
+} // !_parseMarkdownFile
