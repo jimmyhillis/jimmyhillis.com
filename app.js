2ee4cd57f9a05e63c844db69fa351647091cc67a
Jimmy Hillis <jimmy@hillis.me>
2012-12-21 11:42:05 +0800

    Updating all libraries to the latest versions

--- app.js
+++ app.js
@@ -7,35 +7,48 @@ var express = require('express')
   // routes
   , routes = require('./routes')
   , posts = require('./routes/posts')
-  , feeds = require('./routes/feeds');
+  , feeds = require('./routes/feeds')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
+// Build express application
+
+var app = module.exports = express();
 
 // Server configation
 
 app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    // Enable JSONP for my feeds
-    app.set('jsonp callback', true);
-    // Setup variables for caching in my feeds
-    app.set('cache', __dirname + '/cache');
-    app.set('external_cache_time', 600000); // the time to update cache files from external sources
-    // Setup sessions for .flash messages
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'topsecret' }));
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.set('port', process.env.PORT || 3000);
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+
+    // User JADE for markup template lang
+    app.set('view engine', 'jade');
+    app.set('views', path.join(__dirname, 'views'));
+
+    app.use(express.logger('dev'));
+
+    var static = path.join(__dirname, 'public');
+    app.use(express.static(static));
+    app.get(/\/js/, express.static(path.join(static,'js')));
+    app.get(/\/css/, express.static(path.join(static,'css')));
+    app.get(/\/images/, express.static(path.join(static,'images')));
+
+    // app.use(express.static(__dirname + '/public'));
+
+    // Enable JSONP for my feeds
+    // app.set('jsonp callback', true);
+    // app.set('cache', __dirname + '/cache');
+    // app.set('external_cache_time', 600000);
+
+    // app.use(app.router);
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
+    console.log('-- DEVELOPMENT SERVER');
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
     app.use(express.errorHandler());
 });
 
@@ -45,11 +58,6 @@ mongoose.connect('mongodb://localhost/jimmy-hillis-me', function(err) {
     if (err) {
         console.log('Failed to connect to MongoDB');
     }
-});
-
-// Log success cases for testing purposes
-mongoose.connection.on('open', function() {
-    console.log('mongodb is connected');
 });
 
 // Model definitions
@@ -83,18 +91,21 @@ app.get('/music', routes.music);
 app.get('/contact', routes.contact);
 
 // Blog routes
+
 posts = posts(app);
 app.get('/post.:format?', posts.list);
 app.post('/post.:format?', posts.create);
 app.get('/post/:id.:format?', posts.read);
-//app.del('/post/:id.:format?', blogroutes.delete);
-//app.put('/post.:format?', blogroutes.update);
+app.get('/post/:id/edit.:format?', posts.edit);
+app.del('/post/:id.:format?', posts.remove);
+app.put('/post.:format?', posts.update);
 
 // 404 error page
+
 app.use(function(req,res) {
 	res.render('404', { 'title': 'Page Not Found' });
 });
 
-app.listen(3000, function(){
-    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var running = app.listen(3000, function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
