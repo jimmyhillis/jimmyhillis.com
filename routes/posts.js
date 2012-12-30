84cf11c1dc8b307a95de0176521183a20217ae11
Jimmy Hillis <jimmy@hillis.me>
2012-12-30 10:59:57 +0800

    Adding social media share buttons for Twitter and Facebook to each post page

--- routes/posts.js
+++ routes/posts.js
@@ -18,9 +18,9 @@ module.exports = function (app) {
 
 /**
  * List view builds page displaying all existing posts
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.list = function(req, res) {
     Post.find().sort('-posted').find(function (err, posts) {
@@ -39,9 +39,9 @@ controller.list = function(req, res) {
 
 /**
  * Edit view builds form page for editing specific post resource
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.add = function (req, res) {
     res.render('posts/new',
@@ -52,9 +52,9 @@ controller.add = function (req, res) {
 
 /**
  * Create view builds form page for adding a new post resource
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.create = function(req, res) {
     // Create and commit new post
@@ -77,9 +77,9 @@ controller.create = function(req, res) {
 
 /**
  * Read view builds page for a single post in HTML and JSON.
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.read = function (req, res) {
     var id = req.param('id');
@@ -92,6 +92,8 @@ controller.read = function (req, res) {
             if (err || !post) {
                 res.send(404);
             }
+            var headline = post.copy.split("\n")[0]
+              , social_media = _social_media(post, req);
             // Content negotiation
             if (req.params.format === 'json') {
                 return res.json(post);
@@ -99,16 +101,18 @@ controller.read = function (req, res) {
             res.render('posts/post',
                 {
                     'page_title': post.title,
-                    'post': post
+                    'page_description': headline,
+                    'post': post,
+                    'social_media': social_media
                 });
         });
 };
 
 /**
  * Edit view builds form page for editing specific post resource
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.edit = function (req, res) {
     var id = req.param('id');
@@ -164,9 +168,9 @@ controller.update = function (req, res) {
 
 /**
  * Delete post record and redirect back to the post listing
- * @param  {object} req HTTP request object
- * @param  {object} res HTTP response object to return to requester
- * @return {object}     HTTP response render
+ * @param  {express.req} req HTTP request object
+ * @param  {express.res} res HTTP response object to return to requester
+ * @return {object}          HTTP response render
  */
 controller.remove = function (req, res) {
     var id = req.body._id;
@@ -190,3 +194,41 @@ controller.remove = function (req, res) {
             });
         });
 };
+
+/**
+ * Return social media links required for post views
+ * @param  {object}      post Post link for building related `share` URLs
+ * @param  {express.req} req  Express JS request object
+ * @return {object}           Object of social media share links
+ */
+function _social_media(post, req) {
+    var headline = post.copy.split("\n")[0].replace('\r', '');
+    // Share links for social media based on this post
+    var tweet_vars = {
+          'text': '"' + post.title + '" ' + headline,
+          'url': req.current_url,
+          'via': 'ppjim3'
+        }
+      , facebook_vars = {
+          'app_id': '184017585073361',
+          'link': req.current_url,
+          'name': post.title,
+          'description': headline,
+          'redirect_uri': req.current_url
+        }
+      , tweet_link = 'http://twitter.com/intent/tweet'
+                   + '?text=' + encodeURIComponent(tweet_vars.text)
+                   + '&url=' + encodeURIComponent(tweet_vars.url)
+                   + '&via=' + encodeURIComponent(tweet_vars.via)
+      , facebook_link = 'https://www.facebook.com/dialog/feed'
+                      + '?app_id=' + encodeURIComponent(facebook_vars.app_id)
+                      + '&link=' + encodeURIComponent(facebook_vars.link)
+                      + '&name=' + encodeURIComponent(facebook_vars.name)
+                      + '&description=' + encodeURIComponent(facebook_vars.description)
+                      + '&redirect_uri=' + encodeURIComponent(facebook_vars.redirect_uri);
+    // Social media links for the template
+    return {
+        'twitter': tweet_link,
+        'facebook': facebook_link
+    };
+}
